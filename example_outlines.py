b7283d35df1002a1f30dce3f9b9f126defd01b89
Nada Amin <namin@seas.harvard.edu>
2024-02-26 19:25:32 -0500

    more constrained line number

--- example_outlines.py
+++ example_outlines.py
@@ -1,11 +1,14 @@
 import outlines
 
 from cmdline import args
-from test_example import verification_prompt
+from test_example import program, verification_prompt
 
 model = outlines.models.transformers(args.model)
 
-ln = outlines.generate.format(model, int)
+#ln = outlines.generate.format(model, int)
+program_lines = program.split('\n')
+all_program_lines = map(str, range(1, len(program_lines)+1))
+ln = outlines.generate.choice(model, all_program_lines)
 cmd = outlines.generate.choice(model, ["assert", "invariant"])
 prop = outlines.generate.regex(model, r"[^;]+")
 
