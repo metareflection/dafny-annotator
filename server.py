078b3d3d78f5e8eb9b16bb3ee56309b955b9322f
Nada Amin <namin@seas.harvard.edu>
2025-09-27 18:58:26 -0400

    greedy search

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 from typing import List, Optional
 from annotator import propose
 from program import DafnyProgram
-from search import SearchNode, VLLMProposer
+from search import SearchNode, VLLMProposer, batch_greedy_search
 
 proposer = VLLMProposer("gpoesia/dafny-annotator-8B", "meta-llama/Llama-3.1-8B")
 
@@ -11,5 +11,11 @@ app = FastAPI()
 @app.post("/annotate")
 async def annotate(program: str) -> List[str]:
     node = SearchNode(DafnyProgram(program))
-    result = proposer.propose([node])
-    return result[0]
+    results = proposer.propose([node])
+    return results[0]
+
+@app.post("/greedy_search")
+async def search(program: str) -> Optional[str]:
+    programs = [DafnyProgram(program)]
+    results = batch_greedy_search(programs, proposer, 5, None)
+    return results[0]
