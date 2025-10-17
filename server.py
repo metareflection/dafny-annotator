c44e7ce2f9dfcc64562e1794411bec26308b8e9d
Nada Amin <namin@seas.harvard.edu>
2025-10-17 17:41:48 -0400

    support localized server

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from program import DafnyProgram
 from search import SearchNode, VLLMProposer, batch_greedy_search
 import os
 
+LOCALIZED = os.environ.get("LOCALIZED", "false") != "false"
 model_path = os.environ.get("MODEL_PATH", "gpoesia/dafny-annotator-8B")
 proposer = VLLMProposer(model_path, "meta-llama/Llama-3.1-8B")
 
@@ -19,6 +20,6 @@ async def annotate(program: str) -> List[str]:
 @app.post("/greedy_search")
 async def search(program: str) -> Optional[str]:
     programs = [DafnyProgram(program)]
-    results = batch_greedy_search(programs, proposer, 5, None)
+    results = batch_greedy_search(programs, proposer, 5, None, localized=LOCALIZED)
     result = results[0]
     return str(result) if result else None
